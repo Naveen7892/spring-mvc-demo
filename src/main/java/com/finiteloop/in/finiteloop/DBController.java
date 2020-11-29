5a3f4a749dded1054f11eedee59ce0078346b83c
Naveen Kumar <metanaveen@outlook.com>
2020-11-29 22:41:50 +0530

    Making API work

--- src/main/java/com/finiteloop/in/finiteloop/DBController.java
+++ src/main/java/com/finiteloop/in/finiteloop/DBController.java
@@ -9,7 +9,7 @@ import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.RestController;
 
 @RestController
-@RequestMapping(name = "/db")
+@RequestMapping(value = "/db")
 public class DBController {
 	
 	@Autowired
